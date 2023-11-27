cf6b140484df005b878a5979826cf80e2a9685fd
Gerardo Castillo Alva <gerardoalva@outlook.com>
2023-11-26 22:46:26 -0300

    Modification classe product - MongoDB

--- Product/Products.cs
+++ Product/Products.cs
@@ -1,11 +1,24 @@
-﻿namespace CameraReview.Product
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CameraReview.Product
 {
     public class Products : IProduct
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? Id { get; set; }
+
+        [BsonElement("Name")]
         public string Name { get; set; }
+
+        [BsonElement("SKU")]
         public string SKU { get; set; }
+
+        [BsonElement("Manufacturer")]
         public string Manufacturer { get; set; }
+
+        [BsonElement("Features")]
         public List<Feature> Features { get; set; }
 
         public string GetContent()
