fe64c8611ed8851667fa8abfa297dd837351da62
Gerardo Castillo Alva <gerardoalva@outlook.com>
2023-11-28 16:19:46 -0300

    Creating Controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using CameraReview.Product;
+using CameraReview.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CameraReview.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly ProductsServices _productsServices;
+        public ProductsController(ProductsServices productsServices) 
+        {
+            _productsServices = productsServices;
+        }
+
+        [HttpGet]
+        public async Task<List<Products>>GetProducts()
+            => await _productsServices.GetAsync();
+        
+        [HttpPost]
+        public async Task <Products>PostProducts(Products products)
+        {
+            await _productsServices.CreateAsync(products);
+            return products;    
+        }
+
+
+    }
+}
